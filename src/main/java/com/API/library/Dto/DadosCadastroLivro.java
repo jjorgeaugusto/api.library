20aeee3b214364fb45dca8232ffda7debc95d710
jjorgeaugusto <j_xv@hotmail.com>
2023-03-06 19:54:04 -0300

    updated codes

--- src/main/java/com/API/library/Dto/DadosCadastroLivro.java
+++ src/main/java/com/API/library/Dto/DadosCadastroLivro.java
@@ -5,6 +5,4 @@ import com.API.library.Repository.LivroRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 
 public record DadosCadastroLivro(String nome) {
-    public DadosCadastroLivro {
-    }
 }
